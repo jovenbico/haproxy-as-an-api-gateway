200174f54dade855b0a120d3f70ff8858c2d60d9
joven <joven@pahi5>
2021-09-22 16:54:02 +0800

    add authentication service

---
+++ authentication-service/src/test/java/com/sample/authentication/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
